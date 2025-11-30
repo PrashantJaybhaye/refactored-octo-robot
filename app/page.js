d2972b6338571af4c9b3e93d750e28f23351f13d
Prashant <prashan1837d@gmail.com>
2025-12-01 00:51:11 +0530

    feat: Add a new Button UI component, its dependencies, and integrate it into the homepage.

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
-      Subscribe
+      <p>Subscribe</p>
+      <Button>Subscribe</Button>
     </div>
   );
 }
