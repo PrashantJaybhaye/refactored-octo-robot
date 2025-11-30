5b6c78c4874fe8e6f43a4d225933397a0287b233
Prashant <prashan1837d@gmail.com>
2025-12-01 00:42:57 +0530

    feat: Initialize Next.js project with Tailwind CSS, Shadcn UI configuration, and a root layout.

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
         </main>
 
         {/* footer */}
-        <footer>
+        <footer className="bg-muted/50 py-12">
           <div className="container mx-auto px-4 text-center text-gray-200">
             <p>© {new Date().getFullYear()} Doctors Appointment App. All rights reserved.</p>
           </div>
