15afe8b2166ba16c1291c36bb07645ac8db77757
Prashant <prashan1837d@gmail.com>
2025-12-01 01:59:24 +0530

    feat: Implement initial Next.js project setup with UI components and authentication flow.

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,9 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,21 +14,33 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>
-        {/* header */}
+    <ClerkProvider appearance={{
+      baseTheme: dark,
+    }}>
+      <html lang="en" suppressHydrationWarning>
+        <body className={`${inter.className} antialiased`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {/* header */}
+            <Header />
 
-        <main className="min-h-screen">
-          {children}
-        </main>
+            <main className="min-h-screen">
+              {children}
+            </main>
 
-        {/* footer */}
-        <footer className="bg-muted/50 py-12">
-          <div className="container mx-auto px-4 text-center text-gray-200">
-            <p>© {new Date().getFullYear()} Doctors Appointment App. All rights reserved.</p>
-          </div>
-        </footer>
-      </body>
-    </html>
+            {/* footer */}
+            <footer className="bg-muted/50 py-12">
+              <div className="container mx-auto px-4 text-center text-gray-200">
+                <p>© {new Date().getFullYear()} Doctors Appointment App. All rights reserved.</p>
+              </div>
+            </footer>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
+    <div className="py-32">
       <p>Subscribe</p>
       <Button>Subscribe</Button>
     </div>
